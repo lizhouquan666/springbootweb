957b22f1e60834e557d968d57e7f6e2d2c2a0b78
gityouyou <2483028971@qq.com>
2022-01-07 17:05:16 +0800

    html+css+js

---
+++ qian/web/js/myAjax.js
@@ -0,0 +1,27 @@
+$(function () {
+    let loginName = sessionStorage.getItem("loginName");
+    if (loginName == null) {
+        window.location.href = "/html/login.html";
+    }
+});
+
+
+function myAjax(url, data, type) {
+    let res = {};
+    $.ajax({
+        url: url,
+        type: type == null ? 'post' : 'get',
+        data: data,
+        crossDomain:true,
+        xhrFields: {
+            withCredentials: true
+        },
+        async: false,
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            res = data;
+        }
+    });
+    return res;
+}
