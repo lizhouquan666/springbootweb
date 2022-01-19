705ce380474776550a78e8713c320e477e71bc1b
gityouyou <2483028971@qq.com>
2022-01-19 15:19:06 +0800

    注意数据库名字未映射完全《404》

--- qian/web/js/news/news-list.js
+++ qian/web/js/news/news-list.js
@@ -65,7 +65,6 @@ function page(data) {
                 ,{field:'enable', title:'是否启用', width:85, templet: '#switchTpl', unresize: true}
                 , {fixed: 'right', title: '操作', width: 200, align: 'center', toolbar: '#barDemo'}
             ]]
-
             , page: true
         });
     });
